d7d040eac1b013ec3b77743ca07b43d07e3890df
urvish-dholakia <123872529+urvish-dholakia@users.noreply.github.com>
2024-09-06 22:03:35 +0530

    fizzbuzz files created for the fizzbuzz program

---
+++ src/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,3 @@
+def fizzbuzz_str_to_int(input_value: str):
+    if input_value == str(1):
+        input_value = int(input_value)
---
+++ test/fizzbuzz/test_fizzbuzz.py
@@ -0,0 +1,5 @@
+from src.fizzbuzz.fizzbuzz import fizzbuzz_str_to_int
+
+
+def test_str_to_int():
+    assert fizzbuzz_str_to_int("1") == 1
