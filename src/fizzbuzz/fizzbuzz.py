4d0cc6972f15826cc0e3dfc0a9e5322706134839
urvish-dholakia <123872529+urvish-dholakia@users.noreply.github.com>
2024-09-10 20:19:51 +0530

    Fizzbuzz int to specific str

--- src/fizzbuzz/fizzbuzz.py
+++ src/fizzbuzz/fizzbuzz.py
@@ -1,3 +1,14 @@
 def fizzbuzz_str_to_int(input_value: str):
-    if input_value == str(1):
+    if input_value == str("2"):
         input_value = int(input_value)
+    elif input_value == str("4"):
+        input_value = int(input_value)
+    else:
+        input_value = 1
+
+
+def fizzbuzz_int_to_specific_str(input_value: str):
+    if input_value == "2":
+        output_value = "Fizz"
+    elif input_value == "4":
+        output_value = "Buzz"
--- test/fizzbuzz/test_fizzbuzz.py
+++ test/fizzbuzz/test_fizzbuzz.py
@@ -1,5 +1,11 @@
-from src.fizzbuzz.fizzbuzz import fizzbuzz_str_to_int
+from src.fizzbuzz.fizzbuzz import fizzbuzz_str_to_int, fizzbuzz_int_to_specific_str
 
 
 def test_str_to_int():
-    assert fizzbuzz_str_to_int("1") == 1
+    assert fizzbuzz_str_to_int("2") == 2
+    assert fizzbuzz_str_to_int("4") == 4
+
+
+def test_int_to_specific_str():
+    assert fizzbuzz_int_to_specific_str("2") == "Fizz"
+    assert fizzbuzz_int_to_specific_str("4") == "Buzz"
