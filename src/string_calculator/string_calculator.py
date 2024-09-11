463b9f0f01ab01449ffb1d495e6ed729733ac54d
urvish-dholakia <123872529+urvish-dholakia@users.noreply.github.com>
2024-09-11 22:56:41 +0530

    test_upto_6

--- src/string_calculator/string_calculator.py
+++ src/string_calculator/string_calculator.py
@@ -5,8 +5,8 @@ def add(input_value: str):
     if input_value == "":
         input_value = str(0)
     str_del = re.findall(r'\[(.*?)\]', str(input_value))
-    print(str_del)
     str_to_list = re.split("str_del|,|\n|;", input_value)
+    print(str_to_list)
     sum = 0
     for iterator in str_to_list:
         if 0 < int(iterator) < 1000:
