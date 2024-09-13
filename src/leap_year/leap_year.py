4ccf60124da7a1f20a80c368803a37710cd56bd6
urvish-dholakia <123872529+urvish-dholakia@users.noreply.github.com>
2024-09-13 23:24:34 +0530

    leap_year_starting_02

--- src/leap_year/leap_year.py
+++ src/leap_year/leap_year.py
@@ -1,3 +1,7 @@
 def leap_year(input_value: str):
-    if int(input_value) % 4 or int(input_value) % 100 == 0:
+    if int(input_value) % 4 and int(input_value) % 100 != 0:
         return str("Y")
+    if int(input_value) % 400 == 0:
+        return str("Y")
+    else:
+        return str("N")
--- test/leap_year/test_leap_year.py
+++ test/leap_year/test_leap_year.py
@@ -2,4 +2,7 @@ from src.leap_year.leap_year import leap_year
 
 
 def test_leap_year():
-    assert leap_year("1997") == "Y"
+    assert leap_year("1997") == "N"
+    assert leap_year("1996") == "Y"
+    assert leap_year("1600") == "Y"
+    assert leap_year("1800") == "N"
