b62d70bbe783d8e5f7883cd52206457c709c244f
urvish-dholakia <123872529+urvish-dholakia@users.noreply.github.com>
2024-09-14 11:51:38 +0530

    leap_year_completed_01

--- src/leap_year/leap_year.py
+++ src/leap_year/leap_year.py
@@ -1,7 +1,7 @@
 def leap_year(input_value: str):
-    if int(input_value) % 4 and int(input_value) % 100 != 0:
-        return str("Y")
-    if int(input_value) % 400 == 0:
-        return str("Y")
+    if int(input_value) % 400 == 0 and int(input_value) % 100 == 0:
+        return "Y"
+    elif int(input_value) % 4 == 0 and int(input_value) % 100 != 0:
+        return "Y"
     else:
-        return str("N")
+        return "N"
