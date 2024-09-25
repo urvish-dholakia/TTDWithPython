42823cd93a0e0eb745e3e6bdc92bdfb0be21b04b
urvish-dholakia <123872529+urvish-dholakia@users.noreply.github.com>
2024-09-25 18:51:30 +0530

    stack_03

---
+++ src/stack_python/stack_via_function.py
@@ -0,0 +1,3 @@
+def stack_program_push(input_value: str):
+    stack = []
+    stack.append(input_value)
---
+++ test/test_stack_python/test_stack_python.py
@@ -0,0 +1,5 @@
+from src.stack_python.stack_via_function import stack_program_push
+
+
+def test_stack_python():
+    assert stack_program_push("1") == "1"
