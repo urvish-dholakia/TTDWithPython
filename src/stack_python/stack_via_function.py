28170348edf8519ba489eef02e01919e88c3a4de
urvish-dholakia <123872529+urvish-dholakia@users.noreply.github.com>
2024-09-26 10:03:18 +0530

    stack_04

--- src/stack_python/stack_via_function.py
+++ src/stack_python/stack_via_function.py
@@ -1,3 +1,10 @@
 def stack_program_push(input_value: str):
     stack = []
-    stack.append(input_value)
+    values = [v.strip() for v in input_value.split(',')]
+    if len([v for v in values if v]) < 1:
+        stack.append(input_value)
+        return stack[-1]
+    else:
+        input_value_list = input_value.split(",")
+        stack.append(input_value_list)
+        return stack[-1]
--- test/test_stack_python/test_stack_python.py
+++ test/test_stack_python/test_stack_python.py
@@ -3,3 +3,4 @@ from src.stack_python.stack_via_function import stack_program_push
 
 def test_stack_python():
     assert stack_program_push("1") == "1"
+    assert stack_program_push("2,3,4") == "2,3,4"
